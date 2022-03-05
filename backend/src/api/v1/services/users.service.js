c508ccb9074fea2f3c0166bb5d35037d814359b6
SandeepReddyKandi <kandiSandeepReddy98@gmail.com>
2022-03-05 18:56:48 +0530

    adding user service file and integrate validation

---
+++ backend/src/api/v1/services/users.service.js
@@ -0,0 +1,46 @@
+const { User} = require("../models");
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require("bcryptjs");
+
+class UsersService {
+    static async getUserByEmail(email) {
+        if (!email) {
+            throw Error('No email specified');
+        } else {
+            return User.findOne({
+                where: {
+                    email,
+                }
+            });
+        }
+    }
+    static async createUser(userParams) {
+        let { email, firstName, lastName, password } = userParams;
+        // Check the email is not in use already
+        const isEmailUsed = await this.getUserByEmail(email);
+        if (isEmailUsed) {
+            throw Error('email already in use');
+        }
+
+        let passwordHash = null;
+
+        if(password) {
+            passwordHash = await bcrypt.hash(password, 10);
+        }
+        const id =  uuidv4();
+        const createdUser = await User.create({
+            id,
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+        });
+        return User.findOne({
+            where: {
+                id,
+            },
+        });
+    }
+}
+
+module.exports = UsersService;
