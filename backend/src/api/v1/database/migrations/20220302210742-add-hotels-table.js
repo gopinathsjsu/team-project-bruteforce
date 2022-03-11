add4b8f3bc43e38a1fa1f5f951b77e3209e0410b
SandeepReddyKandi <kandiSandeepReddy98@gmail.com>
2022-03-11 23:12:48 +0530

    add hotel db migration

---
+++ backend/src/api/v1/database/migrations/20220302210742-add-hotels-table.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Hotels', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Hotels');
+  }
+};
