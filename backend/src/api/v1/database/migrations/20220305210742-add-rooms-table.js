b2cbc26af389aa48cf993fdba64ba693663b533d
SandeepReddyKandi <kandiSandeepReddy98@gmail.com>
2022-03-12 08:45:48 +0530

    add rooms db migration

---
+++ backend/src/api/v1/database/migrations/20220305210742-add-rooms-table.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Rooms', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      pricePerDay: {
+        type: Sequelize.INTEGER
+      },
+      guestPricePerDay: {
+        type: Sequelize.INTEGER
+      },
+      freeGuestCount: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Rooms');
+  }
+};
