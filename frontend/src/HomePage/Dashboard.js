6bb24808ff9a661d94f225edaf2a083b47728a48
DivyaMaddipudi <divya.maddipudi1@gmail.com>
2022-03-17 00:21:59 -0700

    Searchbar added

--- frontend/src/HomePage/Dashboard.js
+++ frontend/src/HomePage/Dashboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Dashboard.css";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import { AddLocation, AddLocationTwoTone } from "@material-ui/icons";
 
 function Dashboard() {
   return (
@@ -17,6 +19,15 @@ function Dashboard() {
           <p className="logo_des">
             Try searching for a city, a specific hotel, or even a landmark!
           </p>
+        </div>
+        <div className="search_bar">
+          <form className="search_form">
+            <AddLocation />
+            <input
+              type="text"
+              placeholder="Enter a hotel name or destination "
+            />
+          </form>
         </div>
       </div>
     </>
