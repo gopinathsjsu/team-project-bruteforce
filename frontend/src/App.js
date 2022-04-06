ab3b8614ad9529bcd7a1a902f667a4e1577b5b0e
Sai Manasa Yadlapalli <manasa.yadlapalli5@gmail.com>
2022-04-05 19:09:00 -0700

    [Frontend] Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import Home from "./HomePage/Home";
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Navigate,
+  Route
 } from "react-router-dom";
 import LoginPage from "./LoginPage/loginPage";
+import { HotelDetails } from "./HomePage/HotelDetail/HotelDetails";
+import {PaymentMain} from "./HomePage/BookingPayment/PaymentMain";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/hoteldetail/:id"  element={<HotelDetails/>} />
+          <Route path="/payment/:id" element={<PaymentMain />} />
         </Routes>
       </Router>
     </>
