f3963e02e658aadf8cd0c0203b9c0fd430dfce0c
DivyaMaddipudi <divya.maddipudi1@gmail.com>
2022-05-10 15:07:41 -0700

    room count and book room handled in home page

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Modal, Button, Carousel } from "react-bootstrap";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-function Room({ room, fromDate, toDate, guestCount }) {
+function Room({ room, fromDate, toDate, guestCount, roomCount }) {
   const [show, setShow] = useState(false);
 
   console.log(room);
@@ -20,14 +20,16 @@ function Room({ room, fromDate, toDate, guestCount }) {
         </h1>
         <b style={{ fontWeight: "550" }}>
           <p>Location : {room.location}</p>
-          <p>Max Count : {room.maxcount}</p>
           <p>Phone Number : {room.phonenumber}</p>
+          <p>Rooms Remaining : {room.maxcount}</p>
           <p>Type : {room.type}</p>
         </b>
 
         <div style={{ float: "right" }}>
-          {fromDate && toDate && (
-            <Link to={`/book/${room._id}/${fromDate}/${toDate}/${guestCount}`}>
+          {room.maxcount >= roomCount && fromDate && toDate && (
+            <Link
+              to={`/book/${room._id}/${fromDate}/${toDate}/${guestCount}/${roomCount}`}
+            >
               <button
                 className="btn btn-primary m-2"
                 style={{ border: "none" }}
@@ -37,22 +39,38 @@ function Room({ room, fromDate, toDate, guestCount }) {
             </Link>
           )}
 
-          <button
-            className="btn btn-primary"
-            style={{ border: "none", marginRight: "-82px" }}
-            onClick={handleShow}
-          >
-            View Detail
-          </button>
+          {room.maxcount >= 1 ? (
+            <button
+              className="btn btn-primary"
+              style={{
+                border: "none",
+                marginRight: "-82px",
+              }}
+              onClick={handleShow}
+            >
+              View Detail
+            </button>
+          ) : (
+            <button
+              className="btn btn-primary"
+              style={{
+                border: "none",
+                marginRight: "-82px",
+                cursor: "not-allowed",
+              }}
+              onClick={handleShow}
+              disabled
+            >
+              View Detail
+            </button>
+          )}
         </div>
       </div>
 
       <Modal show={show} onHide={handleClose} size="lg">
         <Modal.Header>
           <Modal.Title>
-            <h1 style={{ color: "rgb(252, 158, 21)", fontWeight: "bold" }}>
-              {room.name}
-            </h1>
+            <h1 style={{ color: "black", fontWeight: "bold" }}>{room.name}</h1>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
@@ -71,6 +89,9 @@ function Room({ room, fromDate, toDate, guestCount }) {
           </Carousel>
           <label style={{ fontWeight: "bold" }}>Description:&nbsp;</label>
           <label> {room.description}</label>
+          <br></br>
+          <label style={{ fontWeight: "bold" }}>Rooms Remaining : &nbsp;</label>
+          <label>{room.maxcount}</label>
           <br></br>
           <label style={{ fontWeight: "bold" }}>Rent:&nbsp;</label>
           <label> {room.rentperday}$ per Day</label>
